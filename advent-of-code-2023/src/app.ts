5344b9941e5d645328ed659f82d7acf0199a6c74
Urvish <urvishp45003@gmail.com>
2024-08-23 17:12:36 +0530

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ advent-of-code-2023/src/app.ts
@@ -0,0 +1,23 @@
+import Fastify from "fastify";
+import day01 from "./solutions/day01.js";
+
+const fastify = Fastify({
+    // logger: true,
+});
+
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+fastify.register(day01)
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000, host: '0.0.0.0' })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
---
+++ advent-of-code-2023/src/solutions/day01.ts
@@ -0,0 +1,56 @@
+import { FastifyInstance } from 'fastify';
+import fs from 'fs';
+
+const getFirstAndLastNumber = function (input: string): number[] {
+  const numbers = input
+    .split('')
+    .map(Number)
+    .filter((num) => !isNaN(num));
+  return [numbers[0], numbers[numbers.length - 1]];
+};
+
+const combineNumbers = function (input: number[] | string[]): number {
+  return parseInt(`${input[0]}${input[1]}`);
+};
+
+const getFirstAndLastNumberOrWord = function (input: string): string[] {
+  const wordToNumber: { [key: string]: string } = {
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
+  };
+  const regex = /(?=(1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine))/g;
+
+  const matches = [...input.matchAll(regex)].map(match => match[1]);
+
+  const numbers = matches.map((match) => wordToNumber[match] || match);
+
+  return [numbers[0], numbers[numbers.length - 1]];
+};
+
+export default async function day01(fastify: FastifyInstance): Promise<void> {
+  fastify.get('/01/1', async (request, reply) => {
+    const data = fs.readFileSync('src/input/day01.txt', 'utf-8');
+
+    return data
+      .split('\n')
+      .map(getFirstAndLastNumber)
+      .map(combineNumbers)
+      .reduce((acc, curr) => acc + curr, 0);
+  });
+  fastify.get('/01/2', async (request, reply) => {
+    const data = fs.readFileSync('src/input/day01.txt', 'utf-8');
+
+    return data
+      .split('\n')
+      .map(getFirstAndLastNumberOrWord)
+      .map(combineNumbers)
+      .reduce((acc, curr) => acc + curr, 0);
+  });
+}
